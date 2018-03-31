e122233d7ee24cf8e8164249d67322714411bae2
Nadav Avidan <nadav.avidan@convertro.com>
2018-03-31 15:44:36 +0300

    reset settings route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { SettingsComponent } from './settings';
 export const ROUTES: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'settings', component: NoContentComponent },
   { path: 'signout', component: NoContentComponent },
   { path: '**',    component: NoContentComponent },
 ];
